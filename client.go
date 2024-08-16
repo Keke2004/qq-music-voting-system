7979f4e1f0188b69a150425698a92f9561b1c36e
Keke2004 <168921198+Keke2004@users.noreply.github.com>
2024-08-16 17:12:54 +0800

    Update client.go

--- client.go
+++ client.go
@@ -19,19 +19,15 @@ func main() {
 	}
 	defer conn.Close()
 	c := proto.NewVotingServiceClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	defer cancel()
-	_, err = c.ClearLeaderboard(ctx, &proto.ClearLeaderboardRequest{})
-	if err != nil {
-		log.Fatalf("could not clear leaderboard: %v", err)
-	}
+	ctx := context.Background()
+	clearLeaderboard(ctx, c)
 	rand.Seed(time.Now().UnixNano())
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		for {
-			simulateVoting(c)
+			simulateVoting(ctx, c)
 			time.Sleep(1 * time.Second)
 		}
 	}()
@@ -39,13 +35,20 @@ func main() {
 	go func() {
 		defer wg.Done()
 		for {
-			printLeaderboard(c)
+			printLeaderboard(ctx, c)
 			time.Sleep(5 * time.Second)
 		}
 	}()
 	wg.Wait()
 }
-func simulateVoting(c proto.VotingServiceClient) {
+func clearLeaderboard(ctx context.Context, c proto.VotingServiceClient) {
+	resp, err := c.ClearLeaderboard(ctx, &proto.ClearLeaderboardRequest{})
+	if err != nil {
+		log.Fatalf("could not clear leaderboard: %v", err)
+	}
+	fmt.Printf("ClearLeaderboard response: %s\n", resp.GetMessage())
+}
+func simulateVoting(ctx context.Context, c proto.VotingServiceClient) {
 	var wg sync.WaitGroup
 	voteCount := 100000
 	for i := 0; i < voteCount; i++ {
@@ -54,8 +57,6 @@ func simulateVoting(c proto.VotingServiceClient) {
 			defer wg.Done()
 			starID := int32(rand.Intn(10) + 1)
 			userID := fmt.Sprintf("user_%d", rand.Intn(1000000))
-			ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-			defer cancel()
 			_, err := c.Vote(ctx, &proto.VoteRequest{StarId: starID, UserId: userID})
 			if err != nil {
 				log.Printf("could not vote: %v", err)
@@ -64,9 +65,7 @@ func simulateVoting(c proto.VotingServiceClient) {
 	}
 	wg.Wait()
 }
-func printLeaderboard(c proto.VotingServiceClient) {
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	defer cancel()
+func printLeaderboard(ctx context.Context, c proto.VotingServiceClient) {
 	resp, err := c.GetLeaderboard(ctx, &proto.LeaderboardRequest{})
 	if err != nil {
 		log.Fatalf("could not get leaderboard: %v", err)
