0977fe6ea69247d720ddcc859189171ef0bb58e7
Keke2004 <168921198+Keke2004@users.noreply.github.com>
2024-08-16 11:43:20 +0800

    Update client_test.go

--- client_test.go
+++ client_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"math/rand"
 	"sync"
@@ -12,35 +13,60 @@ import (
 	"google.golang.org/grpc"
 )
 
+const (
+	concurrency  = 100
+	totalRequest = 100000
+)
+
 func BenchmarkVotingService(b *testing.B) {
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-	client := proto.NewVotingServiceClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	c := proto.NewVotingServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	_, err = client.ClearLeaderboard(ctx, &proto.ClearLeaderboardRequest{})
+	_, err = c.ClearLeaderboard(ctx, &proto.ClearLeaderboardRequest{})
 	if err != nil {
-		b.Fatalf("could not clear leaderboard: %v", err)
+		log.Fatalf("could not clear leaderboard: %v", err)
 	}
-	rand.Seed(time.Now().UnixNano())
-	wg := sync.WaitGroup{}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		wg.Add(1)
-		go func() {
+	var wg sync.WaitGroup
+	wg.Add(concurrency)
+	requestsPerGoroutine := totalRequest / concurrency
+	latencies := make([]time.Duration, totalRequest)
+	startTime := time.Now()
+	for i := 0; i < concurrency; i++ {
+		go func(i int) {
 			defer wg.Done()
-			starID := int32(rand.Intn(10) + 1)
-			ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-			defer cancel()
-			_, err := client.Vote(ctx, &proto.VoteRequest{StarId: starID})
-			if err != nil {
-				b.Errorf("could not vote: %v", err)
+			for j := 0; j < requestsPerGoroutine; j++ {
+				starID := int32(rand.Intn(10) + 1)
+				start := time.Now()
+				ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+				defer cancel()
+
+				_, err := c.Vote(ctx, &proto.VoteRequest{StarId: starID})
+				if err != nil {
+					log.Printf("could not vote: %v", err)
+				}
+				latencies[i*requestsPerGoroutine+j] = time.Since(start)
 			}
-		}()
+		}(i)
 	}
 	wg.Wait()
-	b.StopTimer()
+	totalTime := time.Since(startTime)
+	qps := float64(totalRequest) / totalTime.Seconds()
+	var totalLatency time.Duration
+	for _, latency := range latencies {
+		totalLatency += latency
+	}
+	avgLatency := totalLatency / time.Duration(totalRequest)
+	fmt.Printf("Total Requests: %d\n", totalRequest)
+	fmt.Printf("Concurrency: %d\n", concurrency)
+	fmt.Printf("Total Time: %.2fs\n", totalTime.Seconds())
+	fmt.Printf("QPS: %.2f\n", qps)
+	fmt.Printf("Average Latency: %s\n", avgLatency)
+}
+func TestMain(m *testing.M) {
+	BenchmarkVotingService(nil)
 }
